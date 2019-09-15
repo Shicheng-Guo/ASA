11cebf17cf9a0ae7aeb636f8fe5302141c39e406
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-15 14:28:14 -0500

    Update run.sh

--- RA2020/PLD/run.sh
+++ RA2020/PLD/run.sh
@@ -1,4 +1,4 @@
 ```
 cd ~/hpc/rheumatology/RA/he2020
-grep "^PL" ~/hpc/db/hg19/refGene.hg19.V2.bed | awk '{print $1,$2-10000,$3+10000,$5}' OFS="\t" | sort -u | bedtools sort -i | bedtools merge -i - > PLD.hg19.bed
+grep "PL" ~/hpc/db/hg19/refGene.hg19.V2.bed | awk '{print $1,$2-10000,$3+10000,$5}' OFS="\t" | sort -u | bedtools sort -i | bedtools merge -i - > PLD.hg19.bed
 ```
